bca6724b94cb40d2232fc7af3cdec2fa3254228f
HP <vaibhavsrivastava2107@gmail.com>
2020-03-29 09:38:48 +0530

    Preamble Activity Updated.

--- app/src/main/java/com/example/constitutionofindia/Preamble.java
+++ app/src/main/java/com/example/constitutionofindia/Preamble.java
@@ -3,6 +3,7 @@ package com.example.constitutionofindia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Preamble extends AppCompatActivity {
 
@@ -12,5 +13,16 @@ public class Preamble extends AppCompatActivity {
         setContentView(R.layout.activity_preamble);
 
         setTitle("Preamble");
+
+        TextView textView = findViewById(R.id.content);
+
+        textView.setText("WE, THE PEOPLE OF INDIA, having solemnly resolved to constitute India into a SOVEREIGN SOCIALIST SECULAR DEMOCRATIC REPUBLIC and to secure to all its citizens:\n" +
+                "JUSTICE, social, economic and political;\n" +
+                "LIBERTY of thought, expression, belief, faith and worship;\n" +
+                "EQUALITY of status and of opportunity;\n" +
+                "and to promote among them all\n" +
+                "FRATERNITY assuring the dignity of the individual and the unity and integrity of the Nation;\n" +
+                "\n" +
+                "In Our Constituent Assembly this twenty-sixth day of November, 1949, do Hereby Adopt, Enact And Give To Ourselves This Constitution.");
     }
 }
